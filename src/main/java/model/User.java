2d1259e33ff155889500a687f64610d1f9da3593
AD01270149\USER <wooseok01@nhn.com>
2017-11-13 21:56:35 +0900

    [DEV] get방식의 회원가입 기능 구현

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,10 +1,19 @@
 package model;
 
+import java.util.Map;
+
 public class User {
     private String userId;
     private String password;
     private String name;
     private String email;
+
+    public User(Map<String, String> queryMap) {
+        this.userId = queryMap.get("userId");
+        this.password = queryMap.get("password");
+        this.name = queryMap.get("name");
+        this.email = queryMap.get("email");
+    }
 
     public User(String userId, String password, String name, String email) {
         this.userId = userId;
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,16 +1,18 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.util.Map;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+    private static final String WEB_BASE_DIR = System.getProperty("user.dir") + File.separator + "webapp";
 
     private Socket connection;
 
@@ -22,15 +24,45 @@ public class RequestHandler extends Thread {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+        try (InputStream in = connection.getInputStream();
+             OutputStream out = connection.getOutputStream()) {
+
+            InputStreamReader inputStreamReader = new InputStreamReader(in);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            String httpHeader = bufferedReader.readLine();
+            if (httpHeader == null || "".equals(httpHeader)) {
+                return;
+            }
+
+            if (httpHeader.contains("/user/create")) {
+                User user = userParser(httpHeader);
+            }
+
+            if (httpHeader.contains(".html")) {
+                DataOutputStream dos = new DataOutputStream(out);
+
+                byte[] body = htmlParser(httpHeader);
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+                return;
+            }
+
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private User userParser(String httpHeader) {
+        httpHeader = httpHeader.split(" ")[1].replace("/user/create", "").replace("?", "");
+        return new User(HttpRequestUtils.parseQueryString(httpHeader));
+    }
+
+    private byte[] htmlParser(String httpHeader) throws IOException {
+        String htmlFileName = httpHeader.split(" ")[1].replace("/", File.separator);
+
+        File htmlFile = new File(WEB_BASE_DIR + htmlFileName);
+        return Files.readAllBytes(htmlFile.toPath());
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
